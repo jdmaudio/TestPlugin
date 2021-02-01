6dcf68f27ee5c35018702651e731eb19341d837e
David Moore <j.d.moore@gcu.ac.uk>
2021-02-01 15:26:11 +0000

    Added gain control

--- Source/PluginProcessor.cpp
+++ Source/PluginProcessor.cpp
@@ -154,6 +154,8 @@ void TestPluginAudioProcessor::processBlock (juce::AudioBuffer<float>& buffer, j
 
         // ..do something to the data...
     }
+
+    float gain = 0.0f;
 }
 
 //==============================================================================
