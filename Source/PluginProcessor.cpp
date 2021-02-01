ed748cd5f2392f116d886d91d6ecb3a29d026722
David Moore <j.d.moore@gcu.ac.uk>
2021-02-01 15:43:08 +0000

    Revert "Added gain control"
    This reverts commit 6dcf68f27ee5c35018702651e731eb19341d837e.

--- Source/PluginProcessor.cpp
+++ Source/PluginProcessor.cpp
@@ -154,8 +154,6 @@ void TestPluginAudioProcessor::processBlock (juce::AudioBuffer<float>& buffer, j
 
         // ..do something to the data...
     }
-
-    float gain = 0.0f;
 }
 
 //==============================================================================
